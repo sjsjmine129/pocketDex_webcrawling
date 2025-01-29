4d0be6387a987ce2a8475ec4a4037f8a812f1f44
sjsjmine129 <sjsjmine129@gmail.com>
2025-01-29 17:02:58 +0900

    Fix

--- edit.py
+++ edit.py
@@ -8,11 +8,11 @@ with open(file_path, 'r') as file:
 # Add the "image" field to each card based on the id
 for card in data:
     print(card)
-    card['image'] = f"assets/CardImages/{card['id']}.png'),
+    card['image'] = f"assets/CardImages/{card['id']}.png"
 
 # Save the modified data back to the file (or a new file)
 output_file_path = 'cardData_en.json'
 with open(output_file_path, 'w') as output_file:
     json.dump(data, output_file, indent=4)
 
-print(f"Updated file saved as {output_file_path}")
+print("Updated file saved as {output_file_path}")
