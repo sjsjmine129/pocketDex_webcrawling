fa410f4128081512e0645ca1f987875960b92ab8
sjsjmine129 <sjsjmine129@gmail.com>
2025-06-22 11:58:36 +0900

    Fix

--- before_imageMap.js
+++ before_imageMap.js
@@ -1203,6 +1203,16 @@ const imageMap = {
 	100071: require("assets/CardImages/100071.webp"),
 	100072: require("assets/CardImages/100072.webp"),
 	100073: require("assets/CardImages/100073.webp"),
+	100074: require("assets/CardImages/100074.webp"),
+	100075: require("assets/CardImages/100075.webp"),
+	100076: require("assets/CardImages/100076.webp"),
+	100077: require("assets/CardImages/100077.webp"),
+	100078: require("assets/CardImages/100078.webp"),
+	100079: require("assets/CardImages/100079.webp"),
+	100080: require("assets/CardImages/100080.webp"),
+	100081: require("assets/CardImages/100081.webp"),
+	100082: require("assets/CardImages/100082.webp"),
+	100083: require("assets/CardImages/100083.webp"),
 };
 
 export default imageMap;
